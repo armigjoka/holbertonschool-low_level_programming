aee42a36b14cb47935aef89589ca7711c9d0e743
armigjoka <armigjoka090@gmail.com>
2022-06-07 07:51:20 -0700

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -6,15 +6,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main(){
-     int num = 1024
+int main(void)
 
-	while(num ! = 0)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		nu =
-			putchar(c);
-		}
+		putchar (c);
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
