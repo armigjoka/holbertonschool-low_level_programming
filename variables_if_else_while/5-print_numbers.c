a430f55057ab34b789421993d0bbffe6cb2cd951
armigjoka <armigjoka090@gmail.com>
2022-06-07 07:40:20 -0700

    task

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(){
+     int num = 1024
+
+	while(num ! = 0)
+	{
+		nu =
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
