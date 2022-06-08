00df016fd9c74042022221ddbed3bce4ff0b7edd
armigjoka <armigjoka090@gmail.com>
2022-06-08 11:54:21 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ *
+ * Return: void (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
