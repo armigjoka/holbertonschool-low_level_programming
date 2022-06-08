929cd305dee1045dc7efc98923cdfd083208f7a4
armigjoka <armigjoka090@gmail.com>
2022-06-08 07:52:50 -0700

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar(',');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,8 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
-
-
