836dddf9ed8761b4d86774b7d12cfa45a31b5085
armigjoka <armigjoka090@gmail.com>
2022-06-09 11:55:37 -0700

    task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,25 +1,39 @@
 #include "main.h"
+
 /**
  * times_table - Entry
  *
- * Return: void
+ * Return: void (0)
  */
 
 void times_table(void)
 {
-	int i;
-	int j;
+	int i, j, mul;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
+			mul = i * j;
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (mul < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mul + '0');
+			}
+			else if (mul >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
