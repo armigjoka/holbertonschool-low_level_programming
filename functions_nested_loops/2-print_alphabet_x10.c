2784f1565220bd1c651605d00ac50ffd5c8e4181
armigjoka <armigjoka090@gmail.com>
2022-06-09 01:39:27 -0700

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * main - check the code.
+ *
+ * Return: void (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int char;
+
+	for (char = 'a'; char <= 'z'; char++)
+	{
+		_putchar(char);
+	}
+	_putchar('\n');
+}
+
+
