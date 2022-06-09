26e4191d606870b2357d35f12dffc502d3f3c12a
armigjoka <armigjoka090@gmail.com>
2022-06-09 06:47:41 -0700

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit -- entry point
+ * @n: character that the code will check.
+ * Return: 0 or != 0
+ */
+int print_last_digit(int n)
+{
+	int neg;
+
+	neg = (n % 10);
+	if (neg < 0)
+		neg *= -1;
+	_putchar(neg + '0');
+	return (neg);
+}
+
