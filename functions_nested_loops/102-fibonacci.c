0ae3385d242fc795a772f64c84e839b9670bccbf
armigjoka <armigjoka090@gmail.com>
2022-06-10 10:45:01 -0700

    task 14

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -- entry point
+ * Return: 0
+ */
+int main(void)
+{
+	long f1 = 1;
+	long f2 = 2;
+	long fn;
+	int num;
+
+	printf("%li, %li", f1, f2);
+	for (num = 1; num <= 48; num++)
+	{
+		fn = f1 + f2;
+		printf(", %li", fn);
+		f1 = f2;
+		f2 = fn;
+	}
+	printf("\n");
+
+	return (0);
+}
