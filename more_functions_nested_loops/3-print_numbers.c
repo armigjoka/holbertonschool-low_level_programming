df24a64d2afc82b2de8e71e5b92fb287e705a695
armigjoka <armigjoka090@gmail.com>
2022-06-13 04:25:40 -0700

    task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,9 @@
  */
 void print_numbers(void)
 {
-	int i, j;
-
+	int i;
+	
 	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-			_putchar(j + '0');
-		_putchar('\n');
-	}
+		_putchar(i + '0');
+	_putchar('\n');
 }
