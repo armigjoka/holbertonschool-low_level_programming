1c7ddbcb3afac61f7c2b2d3c8e3784da9f1fcd05
armigjoka <armigjoka090@gmail.com>
2022-06-13 06:17:34 -0700

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print more_numbers - Entry
+ * more_numbers - Entry point
  *
- * Return: (0).
+ * Return: 0
  */
 void more_numbers(void)
 {
@@ -14,15 +14,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-			{
-			_putchar(j + '0');
-			}
-			else
+			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
 			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
