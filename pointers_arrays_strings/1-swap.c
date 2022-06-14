68990d1057bae55feeaf510609baf1c5101d4f36
armigjoka <armigjoka090@gmail.com>
2022-06-14 10:05:54 -0700

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Entry
+ * @a: value of integer
+ * @b: value of integer
+ * Return: 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+	*a = 42;
+	*b = 98;
+}
+
