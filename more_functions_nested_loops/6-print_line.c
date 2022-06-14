70d09d52383fca94449dd3173522a4d83cddc76e
armigjoka <armigjoka090@gmail.com>
2022-06-14 01:32:05 -0700

    task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - Entry
+ *
+ * @n : number of lines
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
