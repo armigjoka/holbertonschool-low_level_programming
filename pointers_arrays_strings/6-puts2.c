05503563bd0a15b920a154c84880228f76ca2528
armigjoka <armigjoka090@gmail.com>
2022-06-15 06:19:15 -0700

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string
+ * @str: prints every other character of a string
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int length, j;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < length; j = j + 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
