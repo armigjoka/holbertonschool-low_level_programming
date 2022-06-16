70f16a3a3e18bd80435aff4e9cd4ea8c22c02e60
armigjoka <armigjoka090@gmail.com>
2022-06-16 02:48:26 -0700

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy - entry
+ * @src: function that copies a string
+ * @dest: function tha copy a string
+ * @n: function
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
