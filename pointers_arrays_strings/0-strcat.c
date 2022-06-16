97a209317b2837d8e4f2c4296477493ff3cc6619
armigjoka <armigjoka090@gmail.com>
2022-06-16 01:18:56 -0700

    task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
+	int i, j;
 
-	while (src[i] != '\0')
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		src[i] = dest[i];
 		i++;
+	}
+	while (src[j] != '\0')
+	{
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
