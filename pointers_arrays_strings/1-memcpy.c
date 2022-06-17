140dfdd68969ccf9175a9c530a24d6f62df51c60
armigjoka <armigjoka090@gmail.com>
2022-06-17 03:15:49 -0700

    task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - entry
+ * @n: n bytes
+ * @src: memory area
+ * @dest: memory area
+ *
+ * Return: 0
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i]=src[i];
+	}
+	return (dest);
+}
