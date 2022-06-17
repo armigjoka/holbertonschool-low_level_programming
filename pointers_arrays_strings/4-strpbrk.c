5ef3f8f113c2be6caec41404076d682483825024
armigjoka <armigjoka090@gmail.com>
2022-06-17 05:40:38 -0700

    task 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - entry
+ * @s: string
+ * @accept: string
+ * Return: 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (k = 0; accept[k] != '\0'; k++)
+		{
+			if (s[i] == accept[k])
+				return (s + i);
+		}
+	}
+	return ('\0');
+}
