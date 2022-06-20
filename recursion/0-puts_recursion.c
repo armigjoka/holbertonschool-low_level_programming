87a38636004eb1a96064d6c7482671805bc22b53
armigjoka <armigjoka090@gmail.com>
2022-06-20 04:00:16 -0700

    task 1

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - entry
- * @s: pointer
+ * _print_rev_recursion - entry
+ * @s: string
  * Return void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		s++;
+		_puts_recursion(s);
 	}
 	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	return;
 }
