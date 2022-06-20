c55035bf1c15b26326a0105a8e304797208cb823
armigjoka <armigjoka090@gmail.com>
2022-06-20 03:33:25 -0700

    task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - entry
+ * @s: string
+ * Return void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s--;
+	_print_rev_recursion(s);
+}
