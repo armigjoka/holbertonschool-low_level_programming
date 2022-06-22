8d1ec82386dd7f1f287d0c214ed93296f9b76673
armigjoka <armigjoka090@gmail.com>
2022-06-22 04:41:14 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ * @argc: arg count
+ * @argv: arg array
+ * Return: int
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
