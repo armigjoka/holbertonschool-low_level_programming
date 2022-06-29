c108c88339f0ec4cb6c8b8865c01bb0046097a64
armigjoka <armigjoka090@gmail.com>
2022-06-29 03:02:24 -0700

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,10 @@
 #define _dog_h
 
 /**
- * dog_p - typedef for struct dog
- */
-typedef struct dog dog_t;
-
-/**
  * struct dog - structure for a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * Description: data structure for dog
  */
 struct dog
 {
@@ -19,6 +13,7 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
