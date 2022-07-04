5672f53b9dd1c1fb8d710132cfc23a14848e3219
armigjoka <armigjoka090@gmail.com>
2022-07-04 02:44:22 -0700

    task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -prints the numbers of the function.
+ * @n: numbers
+ * @separator: string to be printed
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list(ap);
+	unsigned int i;
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, unsigned int));
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
