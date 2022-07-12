f525e15983ba8fe381a41ed5e1643f6c20acea7f
armigjoka <armigjoka090@gmail.com>
2022-07-12 10:22:10 -0700

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list
+ * @head: the beginning of the list 
+ * Return: void
+ */
+void free_list(list_t *head)
+{ 
+	list_t *fn;
+
+	while ((fn = head))
+	{
+		head = head->next;
+		free(fn->str);
+		free(fn);
+	}
+}
