71951623bfdc1fbf681bcccfb38489375fb66d8e
armigjoka <armigjoka090@gmail.com>
2022-07-12 09:15:17 -0700

    task 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
  * list_len - the function that return the elemtents in linked list
- * @len: list length
- * Return: the number of elements in linked list
+ * @h: pointer to list
+ * Return: the number of elemtents in linked list
  */
 size_t list_len(const list_t *h)
 {
