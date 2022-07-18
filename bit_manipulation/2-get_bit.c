bd893c4a2eb1849008ffb1eae3973db663b19d1d
armigjoka <armigjoka090@gmail.com>
2022-07-18 11:41:31 -0700

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,10 +9,19 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	
-	if (n >> index & 1)
-		return (1);
-	else
+	unsigned int i = 0;
+
+	if (n == 0 && index < 64)
 		return (0);
-	return (1);
+	i = 0;
+	while (i <= 63)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+		n >>= 1;
+		i++;
+	}
+	return (-1);
 }
