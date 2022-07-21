310ade3ad9e47681b050734c749488318ac688bd
armigjoka <armigjoka090@gmail.com>
2022-07-21 13:08:46 -0700

    task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,45 +1,35 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
  * delete_dnodeint_at_index - structure to print
- * @head: pointer of the list
+ * @h: double pointer to the head
  * @index: index to insert new node
  * Return: 1 or -1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp = *head, *tmp1;
-
+	dlistint_t *tmp = *head;
+	
 	if (*head == NULL)
 		return (-1);
-	if (index == 0)
+	while (index > 0)
 	{
-		if (tmp->next != NULL)
-		{
-			tmp = tmp->next;
-			tmp->prev = NULL;
-		}
-		free(*head);
-		*head = tmp;
-		return (1);
-	}
-	while (index != 0 && tmp->next != NULL)
-	{
+		if (tmp == NULL)
+			return (-1);
 		tmp = tmp->next;
 		index--;
 	}
-	tmp1 = tmp->prev;
-	if (tmp->next == NULL)
+	if (tmp == *head)
 	{
-		tmp1->next = NULL;
-		tmp->prev = NULL;
-		free(tmp);
+		*head = tmp->next;
+		(*head)->prev = NULL;
 	}
 	else
 	{
-		tmp1->next = tmp->next;
-		tmp1 = tmp1->next;
-		tmp1->prev = tmp->prev;
-		free(tmp);
+		tmp->prev->next = tmp->next;
+		if (tmp->next)
+			tmp->next->prev = tmp->prev;
 	}
+	free(tmp);
 	return (1);
 }
