ec87c5c120f7b4b9fd88afb1fa57c9a837312284
armigjoka <armigjoka090@gmail.com>
2022-07-21 13:10:37 -0700

    task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 /**
  * delete_dnodeint_at_index - structure to print
- * @h: double pointer to the head
+ * @head: double pointer to the head
  * @index: index to insert new node
  * Return: 1 or -1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
-	
+
 	if (*head == NULL)
 		return (-1);
 	while (index > 0)
