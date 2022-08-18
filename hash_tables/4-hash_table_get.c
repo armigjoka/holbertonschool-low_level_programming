43beb13d9b3fbcf7016e0188b4617994e85dbafe
armigjoka <armigjoka090@gmail.com>
2022-08-18 06:20:57 -0700

    task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -23,10 +23,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
-		{
 			return (current->value);
 		current = current->next;
-		}
 	}
 	return (NULL);
 }
