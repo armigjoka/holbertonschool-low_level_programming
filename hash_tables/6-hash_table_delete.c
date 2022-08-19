b38eb6a1a307d7f082b2faa7bf9e187beeb8854b
armigjoka <armigjoka090@gmail.com>
2022-08-19 03:04:27 -0700

    task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -18,7 +18,7 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		if (ht->array[j] != NULL)
 		{
-			current = ht->array[i];
+			current = ht->array[j];
 			while (current != NULL)
 			{
 				next = current->next;
