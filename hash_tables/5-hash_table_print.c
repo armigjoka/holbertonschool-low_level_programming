dcb42f1dc0bcba68b68a2974a074fbbe6e9366e9
armigjoka <armigjoka090@gmail.com>
2022-08-19 02:05:14 -0700

    task 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+/**
+ * hash_table_print - function that print a hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int j = 0;
+	hash_node_t *current;
+	short flag = 0;
+
+	if (ht == NULL)
+		return;
+	putchar('{');
+	while (j < ht->size)
+	{
+		if (ht->array[j] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			current = ht->array[j];
+			while (current != NULL)
+			{
+				printf("'%s':'%s'", current->key, current->value);
+				current = current->next;
+				if (current != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+		j++;
+	}
+	putchar('}');
+	putchar('\n');
+}
+
