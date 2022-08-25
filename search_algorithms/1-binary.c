a8a35bcbafb29602efcdf14e5685b49de67b0850
armigjoka <armigjoka090@gmail.com>
2022-08-25 04:41:32 -0700

    task 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -16,10 +16,10 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	printf("Searching in array: ");
 	while (low <= high)
 	{
 		mid = (low + high) / 2;
+		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
 		{
 			printf("%d", array[i]);
